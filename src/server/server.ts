2117194dc50b960a86812470108779a3bfe2757b
mrhorin <u0813mb@gmail.com>
2020-02-12 22:43:58 +0900

    Make it possible to output help

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,19 @@ import JimakuServer from 'server/jimaku_server'
 const server: JimakuServer = new JimakuServer()
 
 let portIdx = global.process.argv.indexOf('-p') + 1
+let helpIdx = global.process.argv.indexOf('-h') + 1
+
+if (helpIdx > 0) {
+  console.log(`
+  Usage:  jimaku-server [option]
+
+  Options
+    -p    specify port for the server (default: 3000)
+    -h    output usage information
+  `)
+  process.exit(0)
+}
+
 if (portIdx > 0) {
   let port: number = Number(global.process.argv[portIdx])
   server.listen(port)
