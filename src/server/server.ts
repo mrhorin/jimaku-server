7bf38d2943679b6c0020db1fad018b4a40075b21
mrhorin <u0813mb@gmail.com>
2020-02-13 20:12:16 +0900

    Add --help and --version option of jimaku-server command

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,32 +1,34 @@
 import JimakuServer from 'server/jimaku_server'
 import path from 'path'
 
-let helpIdx = global.process.argv.indexOf('-h') + 1
-let verIdex = global.process.argv.indexOf('-v') + 1
+let hIdx = global.process.argv.indexOf('-h') + 1
+let helpIdx = global.process.argv.indexOf('--help') + 1
+let vIdx = global.process.argv.indexOf('-v') + 1
+let versionIdx = global.process.argv.indexOf('--version') + 1
 let portIdx = global.process.argv.indexOf('--port') + 1
 let cssIdx = global.process.argv.indexOf('--css') + 1
 let elementIdIdx = global.process.argv.indexOf('--element-id') + 1
 let htmlIdx = global.process.argv.indexOf('--html') + 1
 let jsIdx = global.process.argv.indexOf('--js') + 1
 
-// -v
-if (verIdex > 0) {
+// --version, -v
+if (vIdx > 0 || versionIdx > 0) {
   console.log(process.env.npm_package_version)
   process.exit(0)
 }
-// -h
-if (helpIdx > 0) {
+// --help, -h
+if (hIdx > 0 || helpIdx > 0) {
   console.log(`
   Usage:  jimaku-server [options]
 
   Options:
     --css <path>      specify your css file path
     --element-id <id> specify an id of the target html element when using the default html file (default: jimaku)
-    -h                output usage information
+    --help, -h        output usage information
     --html <path>     specify your html file path
     --js <path>       specify your javascript file path at the client side
     --port <port>     specify port for the server (default: 3000)
-    -v                output output the version number
+    --version, -v     output output the version number
   `)
   process.exit(0)
 }
